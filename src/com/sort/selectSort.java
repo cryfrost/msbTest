0510529ff97b419a38a5939546b53d0a9d411496
zhengwenjun <787410711@qq.com>
2019-06-02 21:29:23 +0800

    排序算法

---
+++ src/com/sort/selectSort.java
@@ -0,0 +1,65 @@
+package com.sort;
+
+/**
+ * @ClassName: DataChecker
+ * @Description: 排序算法对数器
+ * @author Deicide
+ * @date 2019年6月2日 下午8:21:56
+ * 
+ */
+public class selectSort
+{
+	/**
+	 * @Title: ${generateRandomArray} @Description: ${生成数组} @param @return @throws
+	 */
+	public static void main(String[] args)
+	{
+		int arr[] =
+		{ 5, 3, 6, 8, 1, 7, 9, 2, 4 };
+
+		for (int i = 0; i < arr.length - 1; i++)
+		{
+			int minPos = i;
+			for (int j = i + 1; j < arr.length; j++)
+			{
+				if (arr[j] < arr[minPos])
+				{
+					minPos = j;
+				}
+				// minPos=arr[j]<arr[minPos]?j:minPos;
+			}
+			int temp = arr[i];
+			arr[i] = arr[minPos];
+			arr[minPos] = temp;
+
+			System.out.println("minPds:" + minPos);
+			for (int k = 0; k < arr.length; k++)
+			{
+				System.out.print(arr[k] + "  ");
+			}
+		}
+
+	}
+
+	/**
+	 * @Title: ${generateRandomArray} @Description: ${生成数组} @param @return @throws
+	 */
+	public static void sort(int[] arr)
+	{
+		for (int i = 0; i < arr.length - 1; i++)
+		{
+			int minPos = i;
+			for (int j = i + 1; j < arr.length; j++)
+			{
+				if (arr[j] < arr[minPos])
+				{
+					minPos = j;
+				}
+				// minPos=arr[j]<arr[minPos]?j:minPos;
+			}
+			int temp = arr[i];
+			arr[i] = arr[minPos];
+			arr[minPos] = temp;
+		}
+	}
+}
